f92b04988449a5f57b7ee15efe4e387272c32efe
Butcher <phil.nevermind@gmail.com>
2023-01-24 23:52:41 +0100

    Dodanie migracji z PPP do diagnozy

--- Quiz.Data/Migrations/20230124225210_AddDiagnosisCounselingCenter.Designer.cs
+++ Quiz.Data/Migrations/20230124225210_AddDiagnosisCounselingCenter.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Quiz.Data.DataAccess;
 
@@ -11,9 +12,10 @@ using Quiz.Data.DataAccess;
 namespace Quiz.Data.Migrations
 {
     [DbContext(typeof(QuizDbContext))]
-    partial class QuizDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230124225210_AddDiagnosisCounselingCenter")]
+    partial class AddDiagnosisCounselingCenter
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Quiz.Data/Migrations/20230124225210_AddDiagnosisCounselingCenter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Quiz.Data.Migrations
+{
+    public partial class AddDiagnosisCounselingCenter : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PoradniaPsychologiczna",
+                table: "Diagnozy",
+                type: "nvarchar(512)",
+                maxLength: 512,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PoradniaPsychologiczna",
+                table: "Diagnozy");
+        }
+    }
+}
