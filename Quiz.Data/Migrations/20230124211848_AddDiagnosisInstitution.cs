5cec304df3b9c0342579c4ace5d241a641a495ee
Butcher <phil.nevermind@gmail.com>
2023-01-24 22:22:06 +0100

    Dodanie migracji z usunięciem powiązania adresów i uczniów oraz dodaniem placówki oświatowej do diagnozy

---
+++ Quiz.Data/Migrations/20230124211848_AddDiagnosisInstitution.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Quiz.Data.Migrations
+{
+    public partial class AddDiagnosisInstitution : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Uczniowie_Adresy_AdresId",
+                table: "Uczniowie");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Uczniowie_AdresId",
+                table: "Uczniowie");
+
+            migrationBuilder.DropColumn(
+                name: "AdresId",
+                table: "Uczniowie");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PlacowkaOswiatowa",
+                table: "Diagnozy",
+                type: "nvarchar(512)",
+                maxLength: 512,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PlacowkaOswiatowa",
+                table: "Diagnozy");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AdresId",
+                table: "Uczniowie",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Uczniowie_AdresId",
+                table: "Uczniowie",
+                column: "AdresId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Uczniowie_Adresy_AdresId",
+                table: "Uczniowie",
+                column: "AdresId",
+                principalTable: "Adresy",
+                principalColumn: "Id");
+        }
+    }
+}
